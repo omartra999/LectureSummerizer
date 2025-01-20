177df9d6e14ea96055e5f9c03272f7b7e40ae7bc
Omar <omartra069@gmail.com>
2025-01-20 12:26:31 +0100

    extracting audio from video, bug fixes

--- modules/extract_audio.py
+++ modules/extract_audio.py
@@ -1,6 +1,18 @@
 from moviepy import  VideoFileClip
+import os
 
-def extract_audio(input, output):
+def extract_audio(input):
+
+    # getting filename
+    file_name = os.path.basename(input).split(".")[0]
+
+    # getting the audios folder path
+    output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),"../audios"))
+    os.makedirs(output_dir, exist_ok= True)
+
+    # making an output file
+    output = os.path.join(output_dir,f"{file_name}_audio.wav")
+
     video = VideoFileClip(input)
     audio = video.audio
     audio.write_audiofile(output)
